eb146655efba5ce2abf6a5d6ba135b0fc66d4549
Prasanna6725 <147488049+Prasanna6725@users.noreply.github.com>
2023-10-25 22:36:19 +0530

    #include <stdio.h> int main() {     int s,n,f,l;     scanf("%d", &n);     for (f=n;f>= 10;f/= 10)
            l=n%10;
            s=f+l;
         printf("%d", s);
    }

---
+++ Sum_of__First_and_Last_digits_.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main() 
+{
+    int s,n,f,l;
+    scanf("%d", &n);
+    for (f=n;f>= 10;f/= 10)
+    
+        l=n%10;
+        s=f+l;
+     printf("%d", s);
+}
+ 
+
+
+
+
+
+ 
+
+ 
+
+
+
+
+
+ 
