37276387016417ec59418868b9c1d82c5a01a668
Prasanna6725 <147488049+Prasanna6725@users.noreply.github.com>
2023-12-04 21:10:32 +0530

    #include<stdio.h> int main() {     int a,b;     scanf("%d%d",&a,&b);     if(a+b+1==21)     printf("1");     else if(a+b+2==21)     printf("2");     else if(a+b+3==21)     printf("3");     else if(a+b+4==21)     printf("4");     else if(a+b+5==21)     printf("5");     else if(a+b+6==21)     printf("6");     else if(a+b+7==21)     printf("7");     else if(a+b+8==21)     printf("8");     else if(a+b+9==21)     printf("9");     else if(a+b+10==21)     printf("10");     else     printf("-1"); }

---
+++ Blackjack.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    scanf("%d%d",&a,&b);
+    if(a+b+1==21)
+    printf("1");
+    else if(a+b+2==21)
+    printf("2");
+    else if(a+b+3==21)
+    printf("3");
+    else if(a+b+4==21)
+    printf("4");
+    else if(a+b+5==21)
+    printf("5");
+    else if(a+b+6==21)
+    printf("6");
+    else if(a+b+7==21)
+    printf("7");
+    else if(a+b+8==21)
+    printf("8");
+    else if(a+b+9==21)
+    printf("9");
+    else if(a+b+10==21)
+    printf("10");
+    else
+    printf("-1");
+}    
+    
+    
+    
